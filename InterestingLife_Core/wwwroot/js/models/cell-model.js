d1f22de479263514959a69606857af2cb5958362
рома ершов <energy26622662@outlook.com>
2018-12-20 14:02:50 +0600

    disain fix

--- InterestingLife_Core/wwwroot/js/models/cell-model.js
+++ InterestingLife_Core/wwwroot/js/models/cell-model.js
@@ -3,7 +3,7 @@
         var self = this;
         self.cell = ko.observable(val);
         self.cell.subscribe(function (newVal) {
-            var isWord = newVal.match(/[a-zA-Zа-яА-Я\W]/ig);
+            var isWord = newVal.match(/[a-zA-Zа-яА-Я]/ig);
             self.cell(isWord != null ? '' : newVal);
         });
     }
