65672b235b4ccf5c72d8e94aa35797212fa60e95
рома ершов <energy26622662@outlook.com>
2018-12-25 22:03:39 +0600

    fix time widget

--- InterestingLife_Core/wwwroot/js/models/time-view-model.js
+++ InterestingLife_Core/wwwroot/js/models/time-view-model.js
@@ -9,7 +9,7 @@
             var hour = date.getHours();
             var min = date.getMinutes();
             var sec = date.getSeconds();
-            var time = hour + ':' + min + ':' + sec;
+            var time = hour + ':' + ( min.toString().length == 2 ? min : ('0' + min ) ) + ':' + (sec.toString().length == 2 ? sec : ('0' + sec));
             self.time(time);
         }
         setInterval(getCurrentTime, 1000);
