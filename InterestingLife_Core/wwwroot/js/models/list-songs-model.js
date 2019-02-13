9eca0e1d031f935da50166e1386039b6351a8a76
рома ершов <energy26622662@outlook.com>
2019-02-13 20:46:52 +0600

    styles

--- InterestingLife_Core/wwwroot/js/models/list-songs-model.js
+++ InterestingLife_Core/wwwroot/js/models/list-songs-model.js
@@ -10,31 +10,56 @@
             this.lyrics = ko.observable(lyrics);
             this.author = ko.observable(author);
             this.status = ko.observable(status);
+
         }
         self.song = ko.observable();
         self.selectedSong = ko.observableArray();
         self.lyrics = ko.observable();
         self.name = ko.observable();
+        self.categories = ko.observableArray();
 
-        self.getSongs = function() {
-            self.remote.get("https://localhost:5005/api/songs",
-                function (result) {
-                for (var i = 0; i < result.length; i++) {
-                    self.songs.push({id: result[i].id, name: result[i].name});
-                }
-            });
-        }
+        //self.getSongs = function() {
+        //    self.remote.get("https://localhost:5005/api/songs",
+        //        function (result) {
+        //        for (var i = 0; i < result.length; i++) {
+        //            self.songs.push({id: result[i].id, name: result[i].name});
+        //        }
+        //    });
+        //}
 
         self.getSongById = function(song) {
-            debugger;
             self.remote.get("https://localhost:5005/api/songs/"+song.id,
                 function (result) {
-                   // self.song(new Song(result.id, result.name, result.lyrics, result.author, result.status));
                     self.lyrics(result.lyrics);
                     self.name(result.name);
                 });
         }
 
-        self.getSongs();
+        self.getCategories = function() {
+            debugger;
+            self.remote.get("https://localhost:5005/api/categories",
+                function(categories) {
+                    for (var i = 0; i < categories.length; i++) {
+                        self.categories.push({ id: categories[i].id, name: categories[i].name });
+                    }
+                });
+        }
+        self.getSongsByCategory = function (category) {
+            self.remote.get("https://localhost:5005/api/categories/" + category.id,
+                function (result) {
+                    self.reset();
+                    for (var i = 0; i < result.length; i++) {
+                        self.songs.push({ id: result[i].id, name: result[i].name });
+                    }
+                });
+        }
+        self.reset = function() {
+            self.lyrics("");
+            self.name("");
+            self.songs([]);
+        }
+        self.getCategories();
+
+        //  self.getSongs();
     }
 });
